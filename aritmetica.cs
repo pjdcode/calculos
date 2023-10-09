5b0ecaf7705cb70a3981f5e0bbcc9030a900ad94
pjdcode <127801365+pjdcode@users.noreply.github.com>
2023-10-09 23:03:54 +0100

    Adicionado o método Multiplicar

--- aritmetica.cs
+++ aritmetica.cs
@@ -40,6 +40,16 @@ namespace Calculos
             return x-y;
         }
 
+        /// <summary>
+        /// Operação de multiplicação.
+        /// </summary>
+        /// <returns>Retorna o resultado da multiplicação de dois números.</returns>
+        public static int Multiplicar(int x, int y) 
+        {
+            return x * y;
+        }
+
+
 
     }
 }
