ca4f402d732ffcdd8803e4e42c8cfac72b0497fb
pjdcode <127801365+pjdcode@users.noreply.github.com>
2023-10-09 22:49:52 +0100

    Adicionar comentários XML aos métodos somar e subtrair

--- aritmetica.cs
+++ aritmetica.cs
@@ -11,11 +11,19 @@ namespace Calculos
     /// </summary>
     internal class Aritmetica
     {
+        /// <summary>
+        /// operação soma
+        /// </summary>
+        /// <returns>retorna a soma de dois números</returns>
         public int Somar(int x, int y)
         {
             return x+y;
         }
 
+        /// <summary>
+        /// operação substrair
+        /// </summary>
+        /// <returns>retorna a subtração de dois números</returns>
          public int Subtrair(int x, int y)
         {
             return x-y;
